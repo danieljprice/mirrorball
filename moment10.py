f959a53019b149ee93fdd3d39100ea1301af3152
Daniel Price <daniel.price@monash.edu>
2022-09-26 23:43:03 +1000

    implemented PA fitting routine

--- moment10.py
+++ moment10.py
@@ -12,17 +12,15 @@ win = 7
 PA = 154.8 -90. #43.3 # from MAPS (Huang+2018a)
 plt.rcParams['font.size'] = '12'
 
-CO = casa.Cube('~/Observations/exoALMA/CQTau/CQ_Tau_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.image.fits')
+#CO = casa.Cube('~/Observations/exoALMA/CQTau/CQ_Tau_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.image.fits')
 #CO = casa.Cube('~/Observations/exoALMA/DMTau/DM_Tau_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.JvMcorr.fits')
+CO = casa.Cube('~/Observations/HD163296-MAPS/HD_163296_CO_220GHz.robust_0.5_wcont.image.fits')
 
 line_profile = np.nansum(CO.image, axis=(1,2)) / CO._beam_area_pix()
-#smoothCO = interpolate.Akima1DInterpolator(CO.velocity,np.nan_to_num(CO.image))
-
-#CO = casa.Cube('~/Observations/HD163296-MAPS/HD_163296_CO_220GHz.robust_0.5_wcont.image.fits')
 
 iv0 = np.abs(CO.velocity - Vsyst).argmin()
 #iv0 = 63 #np.abs(CO.velocity - Vsyst).argmin()
-print("systemic channel is ",iv0," v = ",CO.velocity[iv0])
+print("systemic channel is ",iv0," v = ",CO.velocity[iv0], ' max v is ',np.max(CO.velocity))
 
 def flip_it(im,PA):
     #rotate to major axis, flip and rotate back
@@ -38,18 +36,27 @@ def mirror_line_profile_and_get_error(vsys):
     # shift everything so that systemic velocity corresponds to v=0
     vgrid = CO.velocity - vsys
 
+    # flip velocity grid to increasing order if necessary
+    if (vgrid[len(vgrid)-1] < vgrid[0]):
+       print("velocity grid is inverted -> flipping")
+       vgrid = np.flip(vgrid)
+       line_profile_local = np.flip(line_profile)
+    else:
+       line_profile_local = line_profile
+
     # plot original line profile
-    plt.xlim(np.min(vgrid),np.max(vgrid))
-    plt.plot(vgrid,line_profile)
+    fig,ax = plt.subplots()
+    ax.set_xlim(np.min(vgrid),np.max(vgrid))
+    ax.plot(vgrid,line_profile_local)
 
     # get profile mirrored across the systemic velocity
     neg_channels = vgrid < 0.
     pos_channels = np.logical_not(neg_channels)
-    pr = line_profile[pos_channels]
+    pr = line_profile_local[pos_channels]
     vr = vgrid[pos_channels]
 
     # reflect the negative channels across the v=0 axis
-    pl = np.flip(line_profile[neg_channels])
+    pl = np.flip(line_profile_local[neg_channels])
     vl = np.flip(np.abs(vgrid[neg_channels]))
 
     # get smoothed versions of both profiles
@@ -60,15 +67,17 @@ def mirror_line_profile_and_get_error(vsys):
     prs = sr(xs)
 
     # plot smoothed and reflected profiles and the difference
-    plt.plot(xs,pls)
-    plt.plot(xs,prs)
-    plt.plot(xs,prs-pls)
+    ax.plot(xs,pls)
+    ax.plot(xs,prs)
+    ax.plot(xs,prs-pls)
 
     # compute the error between the two
     err2 = np.nansum((prs-pls)**2)/np.size(xs)
     filename='lineprofile-vsys-%f.png' % (vsys)
     print("vys = ",vsys,"err2 = ",err2," -> ",filename)
+    #plt.show()
     plt.savefig(filename)
+    plt.close(fig)
     return err2
 
 def get_vsyst_from_line_profile():
@@ -76,6 +85,84 @@ def get_vsyst_from_line_profile():
     res = optimize.minimize(mirror_line_profile_and_get_error,x0,bounds=[[Vsyst-3.,Vsyst+3.]],method='Nelder-Mead')
     print('finished, got vsys = ',res.x,' with error ',res.fun)
     return res.x
+
+def mirror_systemic_channel_and_get_error(PA,iv,plot=False):
+    channel = np.nan_to_num(CO.image[iv,:,:])
+    mirror  = flip_it(channel,PA[0])
+    err = np.mean((channel-mirror)**2)
+    print(" channel ",iv," trying PA=",PA,", error is ",err)
+
+    if (plot):
+       # make the plot
+       fig, axes = plt.subplots(1,3, figsize=(21,8), sharex='all', sharey='all', num=win)
+       axes[0].imshow(channel,vmin=0.,vmax=0.04,cmap='viridis',origin='lower')
+       axes[1].imshow(mirror,vmin=0.,vmax=0.04,cmap='viridis',origin='lower')
+       axes[2].imshow(channel-mirror,vmin=-0.01,vmax=0.01,cmap='inferno_r',origin='lower')
+       plt.show()
+
+    return err
+
+def mirror_channel_and_get_error(PA,iv,iv0,plot=False):
+    i = iv - iv0 + int(nv/2)
+    iv_sym = iv0 + (nv-1) - int(nv/2) - i
+    print('channel ',iv,' goes with ',iv_sym)
+    cminus = np.nan_to_num(CO.image[iv,:,:])
+    cplus = np.nan_to_num(CO.image[iv_sym,:,:])
+
+    cplusr = flip_it(cplus,PA[0])
+    cminusr = flip_it(cminus,PA[0])
+
+    if (plot):
+       fig, axes = plt.subplots(1,3, figsize=(21,8), sharex='all', sharey='all', num=win)
+       axes[0].imshow(cminus,vmin=0.,vmax=0.04,cmap='viridis',origin='lower')
+       axes[1].imshow(cplusr,vmin=0.,vmax=0.04,cmap='viridis',origin='lower')
+       axes[2].imshow(cplusr-cminus,vmin=-0.01,vmax=0.01,cmap='inferno_r',origin='lower')
+       plt.savefig('channel%i.png' % i)
+       plt.pause(0.5)
+
+    err = np.sqrt(np.mean((cplusr-cminus)**2))
+    print(" channel ",iv," trying PA=",PA,", rms residual is ",err)
+    return err
+
+def get_PA(PA0,iv0):
+    x0 = np.array(0.)
+    offset = 12
+    #mirror_channel_and_get_error(x0,iv0+6,iv0,plot=True)
+    #res = optimize.minimize(mirror_systemic_channel_and_get_error,x0,args=(iv0),bounds=[[-90.,90.]],method='nelder-mead',tol=0.01)
+    #res = optimize.minimize(mirror_channel_and_get_error,x0,args=(iv0+offset,iv0),bounds=[[-90.,90.]],method='dogleg')
+    #res = optimize.dual_annealing(mirror_channel_and_get_error,bounds=[[-90.,90.]],args=(iv0+offset,iv0))
+    #
+    # brute force search for PA to nearest 5 degrees
+    #
+    res = optimize.brute(mirror_systemic_channel_and_get_error,args=([iv0]),Ns=30,ranges=[[0.,180.]],finish=None)
+#    kwargs = { 'method':'nelder-mead' }
+#    res = optimize.basinhopping(func=lambda x, y=iv0:mirror_systemic_channel_and_get_error(x,y),x0=x0,stepsize=30.,minimizer_kwargs=kwargs)
+    #res = optimize.dual_annealing(mirror_systemic_channel_and_get_error,args=([iv0]),bounds=[[-90.,90.]])
+    #res = optimize.shgo(func=lambda x, y=iv0+offset,z=iv0:mirror_channel_and_get_error(x,y,z),bounds=[[-90.,90.]])
+    print("brute force search gives PA = ",res)
+    PA = res #.x[0]
+    err = mirror_systemic_channel_and_get_error([PA],iv0,plot=False)
+    x0 = PA
+    res = optimize.minimize(mirror_systemic_channel_and_get_error,x0,args=(iv0),method='nelder-mead')
+    print('finished, got PA = ',PA,' with error ',err)
+    mirror_systemic_channel_and_get_error(res.x,iv0,plot=True)
+    # try flipped 90 degrees
+    PA = res.x[0]
+    x0 = PA-90.
+    res2 = optimize.minimize(mirror_systemic_channel_and_get_error,x0,args=(iv0),bounds=[[-90.,90.]],method='nelder-mead')
+    #res2 = optimize.minimize(mirror_channel_and_get_error,x0,args=(iv0+offset,iv0),bounds=[[-90.,90.]],method='nelder-mead')
+    #res = optimize.shgo(func=lambda x, y=iv0:mirror_systemic_channel_and_get_error(x,y),bounds=[[-90.,90.]])
+    #res2 = res
+    #res2.x[0] = res.x[0] - 90.
+    #mirror_channel_and_get_error(res2.x,iv0+offset,iv0,plot=True)
+    print('finished, got PA = ',res2.x,' with error ',res2.fun)
+    err1 = mirror_channel_and_get_error(res.x,iv0+offset,iv0,plot=True)
+    err2 = mirror_channel_and_get_error(res2.x,iv0+offset,iv0,plot=True)
+    print('comparison, got ',err1,err2,' for PA=',res.x[0],res2.x[0])
+    if (err2 < err1):
+       PA = res2.x[0]
+
+    return PA
 
 def get_vsyst(PA):
     errmin = 1e8
@@ -94,12 +181,15 @@ def get_vsyst(PA):
         print("error is ",err,' systemic channel is',iv00,' with v=',CO.velocity[iv00])
     return CO.velocity[iv00]
 
-vsyst = get_vsyst_from_line_profile()
+Vsyst = get_vsyst_from_line_profile()
+iv0 = np.abs(CO.velocity - Vsyst).argmin()
+print("closest channel to systemic is ",iv0," v = ",CO.velocity[iv0],CO.velocity[iv0+1])
+PA = get_PA(PA,iv0)
+print("PA is ",PA," degrees")
 
 map = np.zeros(np.shape(CO.image[iv0,:,:]))
 for i in range(int(nv/2)+1):
     iv = iv0 + i - int(nv/2)
-
     iv_sym = iv0+(nv-1)-int(nv/2)-i
     print('channel ',iv,' goes with ',iv_sym)
     cminus = np.nan_to_num(CO.image[iv,:,:])
