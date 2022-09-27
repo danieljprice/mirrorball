a352502a76122616a35369b90f3091f982bfc493
Daniel Price <daniel.price@monash.edu>
2022-09-27 16:16:58 +1000

    various fit values stored as comments; working routine; added docstring comments

--- moment10.py
+++ moment10.py
@@ -9,25 +9,35 @@ import scipy.optimize as optimize
 win = 7
 plt.rcParams['font.size'] = '12'
 
+CO = casa.Cube('~/Observations/exoALMA/SYCha/SY_Cha_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.image_denoise.fits')
+#CO = casa.Cube('~/Observations/exoALMA/SYCha/SY_Cha_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.JvMcorr.fits')
+#CO = casa.Cube('~/Observations/IMLup/MAPS/IM_Lup_CO_220GHz.robust_0.5_wcont.JvMcorr.image.fits')
+#CO = casa.Cube('~/Observations/PDS70/PDS70_COcube_robust-0.5_uvtaper0.03.fits')
 #CO = casa.Cube('~/Observations/exoALMA/CQTau/CQ_Tau_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.image.fits')
 #CO = casa.Cube('~/Observations/exoALMA/DMTau/DM_Tau_12CO_robust0.5_width0.1kms_threshold4.0sigma.clean.JvMcorr.fits')
-CO = casa.Cube('~/Observations/HD163296-MAPS/HD_163296_CO_220GHz.robust_0.5_wcont.image.fits')
+#CO = casa.Cube('~/Observations/HD163296-MAPS/HD_163296_CO_220GHz.robust_0.5_wcont.image.fits')
 
 nv = len(CO.velocity)
 line_profile = np.nansum(CO.image, axis=(1,2)) / CO._beam_area_pix()
 
 def flip_it(im,PA):
+    """
+       rotate a velocity channel to match the channel on the other side of the disc
+    """
     #rotate to major axis, flip and rotate back
     im = np.fliplr(ndimage.rotate(im,PA+90.,reshape=False))
     return ndimage.rotate(im,-PA-90.-180.,reshape=False)
 
 def get_channel(v):
+    """
+       get channel corresponding to specified velocity, by interpolating from neighbouring channels
+    """
     iv = np.abs(CO.velocity - v).argmin()
 
     # do not interpolate past bounds of the array, ends just return end channel
     ivp1 = iv+1
     ivm1 = iv-1
-    if (ivp1 > len(CO.velocity) or ivm1 < 0):
+    if (ivp1 > len(CO.velocity)-1 or ivm1 < 0):
        return CO.image[iv,:,:],iv,iv
 
     # deal with channels in either increasing or decreasing order
@@ -45,6 +55,15 @@ def get_channel(v):
     return c1*(1.-x) + x*c2,iv,iv1
 
 def mirror_line_profile_and_get_error(vsys,plot=False):
+    """
+       reflect the line profile across an axis corresponding to the systemic velocity
+       and subtract one side from the other to get the residual. The idea is that
+       the systemic velocity is the one which minimises this residual
+    """
+    #rotate to major axis, flip and rotate back
+    im = np.fliplr(ndimage.rotate(im,PA+90.,reshape=False))
+    return ndimage.rotate(im,-PA-90.-180.,reshape=False)
+
     # shift everything so that systemic velocity corresponds to v=0
     vgrid = CO.velocity - vsys
 
@@ -92,14 +111,21 @@ def mirror_line_profile_and_get_error(vsys,plot=False):
 
     return err2
 
-def get_vsyst_from_line_profile():
+def get_vsyst_from_line_profile(Vsyst):
+    """
+       fit for the systemic velocity (defined here as the symmetry axis for the line profile)
+    """
     x0 = np.array([Vsyst])
     res = optimize.minimize(mirror_line_profile_and_get_error,x0,bounds=[[Vsyst-3.,Vsyst+3.]],method='Nelder-Mead')
-    #mirror_line_profile_and_get_error(res.x,plot=True)
+    mirror_line_profile_and_get_error(res.x,plot=True)
     print('finished, got vsys = ',res.x,' with error ',res.fun)
     return res.x
 
 def mirror_systemic_channel_and_get_error(PA,iv,plot=False,image=None):
+    """
+       mirror the systemic velocity channel, which in
+       a symmetric disc should be the reflection of itself
+    """
     # optional argument image can be used to send an interpolated channel here
     if (image is not None):
        channel = image
@@ -121,14 +147,23 @@ def mirror_systemic_channel_and_get_error(PA,iv,plot=False,image=None):
     return err
 
 def mirror_at_vsys(vsys,PA,plot=False):
+    """
+       mirror the image at the systemic velocity (using an interpolated channel)
+    """
     image,iv,iv1 = get_channel(vsys)
     return mirror_systemic_channel_and_get_error(PA,iv,image=image,plot=plot)
 
 def mirror_at_vsys_PA(PA,vsys,plot=False):
+    """
+       as above but takes arguments in different order for use in optimisation
+    """
     image,iv,iv1 = get_channel(vsys)
     return mirror_systemic_channel_and_get_error(PA,iv,image=image,plot=plot)
 
 def mirror_channel_and_get_error(PA,iv,iv0,plot=False):
+    """
+       compare and subtract a channel from its mirror pair across the symmetry axis
+    """
     i = iv - iv0 + int(nv/2)
     iv_sym = iv0 + (nv-1) - int(nv/2) - i
     cminus = np.nan_to_num(CO.image[iv,:,:])
@@ -152,6 +187,9 @@ def mirror_channel_and_get_error(PA,iv,iv0,plot=False):
     return err
 
 def get_PA(iv0,vsys,PA0=None):
+    """
+       fitting procedure to obtain the position angle of the symmetry axis
+    """
     x0 = np.array(0.)
     offset = 12
     if (PA0 is not None):
@@ -166,8 +204,9 @@ def get_PA(iv0,vsys,PA0=None):
        # try flipped 90 degrees
        #
        PA2 = PA-90.
-       #res2 = optimize.minimize(mirror_systemic_channel_and_get_error,x0,args=(iv0),bounds=[[-90.,90.]],method='nelder-mead')
-       #print('finished, got PA = ',res2.x,' with error ',res2.fun)
+       res2 = optimize.minimize(mirror_systemic_channel_and_get_error,[PA2],args=(iv0),bounds=[[-90.,90.]],method='nelder-mead')
+       print('finished, got PA = ',res2.x,' with error ',res2.fun)
+       PA2 = res2.x[0]
        #
        # solve the 90 degree uncertainty with a channel offset from the systemic channel
        #
@@ -189,12 +228,19 @@ def get_PA(iv0,vsys,PA0=None):
     return res.x[0]
 
 def get_vsyst_from_channels(Vsys0,PA):
+    """
+       fit for the systemic velocity by reflecting the image of the systemic channel about the symmetry axis
+       this should give more precise results than just reflecting the line profile
+    """
     res = optimize.minimize(mirror_at_vsys,Vsys0,args=(PA),bounds=[[Vsys0-0.2,Vsys0+0.2]],method='nelder-mead')
     print('finished, got vsys = ',res.x,' with error ',res.fun)
     mirror_at_vsys(res.x,PA,plot=True)
     return res.x
 
 def plot_interpolated_channel(vsys):
+    """
+       sanity check: plot an interpolated channel compared to the two neighbouring channels
+    """
     im,iv,iv1 = get_channel(vsys)
     fig, axes = plt.subplots(1,3, figsize=(21,8), sharex='all', sharey='all', num=win)
     axes[0].set_title('V = {}'.format(CO.velocity[iv]))
@@ -207,6 +253,9 @@ def plot_interpolated_channel(vsys):
     return
 
 def mirror_cube(Vsys,PA,plot=False):
+    """
+       compute the residual of a line cube by subtracting the mirror opposite of each channel
+    """
     iv0 = np.abs(CO.velocity - Vsys).argmin()
     cube = np.zeros(np.shape(CO.image[:,:,:]))
     #map = np.zeros(np.shape(CO.image[iv0,:,:]))
@@ -238,34 +287,50 @@ def mirror_cube(Vsys,PA,plot=False):
     return cube
 
 def get_vsys_and_PA():
-    Vsyst = get_vsyst_from_line_profile()
+    """
+       fit both the systemic velocity and position angle
+       with increasing degrees of sophistication
+    """
+    Vsyst = np.mean(CO.velocity)  # a wild guess
+    Vsyst = get_vsyst_from_line_profile(Vsyst)
     iv0 = np.abs(CO.velocity - Vsyst).argmin()
     print("closest channel to systemic is ",iv0," v = ",CO.velocity[iv0],CO.velocity[iv0+1])
     #plot_interpolated_channel(Vsyst)
 
-    #PA=43.3
     #Vsyst = get_vsyst_from_channels(Vsyst,PA)
 
-    PA = get_PA(iv0,Vsyst,PA0=45.)
+    PA = get_PA(iv0,Vsyst)
     print("PA is ",PA," degrees")
 
     Vsyst = get_vsyst_from_channels(Vsyst,[PA])
     PA = get_PA(iv0,Vsyst,PA0=PA)
     print("refined PA is ",PA," degrees")
-    return Vsyst,PA
+    return Vsyst[0],PA
 
-(Vsyst,PA) =(5.763665,42.343013)
-#Vsyst,PA = get_vsys_and_PA()
-print(" USING Vsyst = %f, PA=%f " %(Vsyst,PA))
+(Vsyst,PA) = get_vsys_and_PA()
+
+#(Vsyst,PA) = (4.126982,74.243033) # SY Cha denoise
+#(Vsyst,PA) = (4.147810,74.062892) # SY Cha
+#(Vsyst,PA) = (4.502893,54.819273) # IM Lup
+#(Vsyst,PA) = (4.4,53.) # IM Lup literature
+#(Vsyst,PA) = (5.715284,66.469651) # PDS 70
+#(Vsyst,PA) = (6.220097,145.735504) # CQ Tau
+#(Vsyst,PA) =(5.763665,42.343013) # HD 163296
+#(Vsyst,PA) = (6.045766,65.414138) # DM Tau
+print(" USING (Vsyst,PA) = (%f,%f) " %(Vsyst,PA))
 
 CO.image = mirror_cube(Vsyst,PA,plot=True)
 print("writing to mirror.fits...")
 CO.writeto('mirror.fits')
 
-CO.plot(moment=0,fmin=-0.002,fmax=0.002,limit=0.3,cmap='inferno')
-plt.savefig('HD163-moment10.png',bbox_inches='tight')
+CO.plot(moment=0,fmin=-0.005,fmax=0.005,limit=5.,cmap='inferno')
+plt.savefig('mirror-moment0.png',bbox_inches='tight')
 plt.show()
 
-CO.plot(moment=1,limit=0.3,cmap='RdBu_r')
-plt.savefig('HD163-moment101.png',bbox_inches='tight')
+CO.plot(moment=1,limit=5.,fmin=Vsyst-2.,fmax=Vsyst+2.,cmap='RdBu_r')
+plt.savefig('mirror-moment1.png',bbox_inches='tight')
+plt.show()
+
+CO.plot(moment=9,limit=5.,fmin=Vsyst-2.,fmax=Vsyst+2.,cmap='RdBu_r')
+plt.savefig('mirror-moment9.png',bbox_inches='tight')
 plt.show()
